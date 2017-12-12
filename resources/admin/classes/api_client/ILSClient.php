828302de18e849d30dc708c9b8a0a8dc9d8c94af
Matthias Meusburger <matthias.meusburger@biblibre.com>
2017-12-12 09:25:27 +0100

    Update interface

--- resources/admin/classes/api_client/ILSClient.php
+++ resources/admin/classes/api_client/ILSClient.php
@@ -2,8 +2,12 @@
 
 interface ILSClient {
     function getFunds();
+    function getFund($fundid);
     function getILSName();
     function getILSURL();
+    function placeOrder($order);
+    function updateOrder($order);
+    function getOrder($orderid);
 }
 
 ?>
